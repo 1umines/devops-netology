30636da70dabcac4fbf479e8e7cec8bb7f48db4e
Kravtsov Egor <luminesx@gmail.com>
2021-11-02 20:24:07 +0300

    rebase 02.11

--- 02-git-03-branching/branching/rebase.sh
+++ 02-git-03-branching/branching/rebase.sh
@@ -2,10 +2,6 @@
 # display command line options
 count=1
 for param in "$@"; do
-<<<<<<< HEAD
     echo "\$@ Parameter #$count = $param"
-=======
-    echo "Parameter: $param"
->>>>>>> dc4688f... git 2.3 rebase @ instead *
     count=$(( $count + 1 ))
 done
